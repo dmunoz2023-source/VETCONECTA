55d0d9eaa9b712c6fe01110a45282e290c0d1772
decoddee <jeremy.joaquin06@gmail.com>
2026-09-20 21:23:31 -0300

    chore: guardar estado previo a la sincronizacion de workspaces

--- apps/backend/clients-pets-service/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PetsModule } from './modules/pets/pets.modules';
-@Module({
-  imports: [PetsModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
