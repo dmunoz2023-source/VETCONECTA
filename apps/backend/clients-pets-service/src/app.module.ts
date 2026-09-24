ab9cad51f3ad2af71f19f34c326f93c3a9b4b81b
lenhino <lchavez2025@alu.uct.cl>
2026-09-24 16:27:50 -0300

    feat(clients-pets): registrar DatabaseModule, ClientsModule y PetsModule

--- apps/backend/clients-pets-service/src/app.module.ts
+++ apps/backend/clients-pets-service/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { validationSchema } from './config/validation.schema';
+import { DatabaseModule } from './database/database.module';
+import { ClientsModule } from './modules/clients/clients.module';
+import { PetsModule } from './modules/pets/pets.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
+      validationSchema,
     }),
+    DatabaseModule,
+    ClientsModule,
+    PetsModule,
   ],
   controllers: [],
   providers: [],
